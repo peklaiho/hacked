8860e6af7dcd440d11f648df9131b9940ea79e2b
Pekka Laiho <pekka.i.laiho@gmail.com>
2021-07-12 09:57:57 +0700

    add file for signals, not used yet

---
+++ signal.scm
@@ -0,0 +1,31 @@
+(define SIGHUP     1) ; Hangup
+(define SIGINT     2) ; Interactive attention signal
+(define SIGQUIT    3) ; Quit
+(define SIGILL     4) ; Illegal instruction
+(define SIGTRAP    5) ; Trace/breakpoint trap
+(define SIGABRT    6) ; Abnormal termination
+(define SIGBUS     7) ; Bus error
+(define SIGFPE     8) ; Erroneous arithmetic operation
+(define SIGKILL    9) ; Killed
+(define SIGUSR1   10) ; User-defined signal 1
+(define SIGSEGV   11) ; Invalid access to storage
+(define SIGUSR2   12) ; User-defined signal 2
+(define SIGPIPE   13) ; Broken pipe
+(define SIGALRM   14) ; Alarm clock
+(define SIGTERM   15) ; Termination request
+(define SIGSTKFLT 16) ; Stack fault (obsolete)
+(define SIGCHLD   17) ; Child terminated or stopped
+(define SIGCONT   18) ; Continue
+(define SIGSTOP   19) ; Stop, unblockable
+(define SIGTSTP   20) ; Keyboard stop
+(define SIGTTIN   21) ; Background read from control terminal
+(define SIGTTOU   22) ; Background write to control terminal
+(define SIGURG    23) ; Urgent data is available at a socket
+(define SIGXCPU   24) ; CPU time limit exceeded
+(define SIGXFSZ   25) ; File size limit exceeded
+(define SIGVTALRM 26) ; Virtual timer expired
+(define SIGPROF   27) ; Profiling timer expired
+(define SIGWINCH  28) ; Window size change
+(define SIGPOLL   29) ; Pollable event occurred (System V)
+(define SIGPWR    30) ; Power failure imminent
+(define SIGSYS    31) ; Bad system call
